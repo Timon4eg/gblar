3dba05175b4b34b14f41d290a7ffb64b6bd554fa
Timon4eg <62251008+Timon4eg@users.noreply.github.com>
2021-11-01 12:39:20 +0300

    Route group

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,15 @@ use App\Http\Controllers\Admin\IndexController;
 Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/about', [MainController::class, 'about'])->name('about');
 
-Route::get('/news', [NewsController::class, 'index'])->name('news');
-Route::get('/news/{id}', [NewsController::class, 'show'])->where('id', '[0-9]+')->name('show');
+Route::prefix('news')
+->group(function() {
+Route::get('/', [NewsController::class, 'index'])->name('news');
+Route::get('/{id}', [NewsController::class, 'show'])->where('id', '[0-9]+')->name('show');
+});
 
-Route::get('/admin', [IndexController::class, 'index'])->name('admin-main');
-Route::get('/admin/test1', [IndexController::class, 'test1'])->name('test1');
-Route::get('/admin/test2', [IndexController::class, 'test2'])->name('test2');
+Route::prefix('admin')
+->group(function() {
+    Route::get('/', [IndexController::class, 'index'])->name('admin-main');
+    Route::get('/test1', [IndexController::class, 'test1'])->name('test1');
+    Route::get('/test2', [IndexController::class, 'test2'])->name('test2');
+});
