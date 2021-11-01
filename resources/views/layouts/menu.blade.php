187656b42041c41dfe3f108078bf70f7d299918e
Timon4eg <62251008+Timon4eg@users.noreply.github.com>
2021-11-01 13:57:03 +0300

    menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,12 +2,8 @@
 <ul class="top-menu left-menu">
     <li class="top-menu-item"><a href="{{ route ('index') }}">Главная</a></li>
     <li class="top-menu-item"><a href="{{ route ('about') }}">О нас</a></li>
-    <li class="top-menu-item"><a href="{{ route ('news') }}">Новости</a>
-        <ul class="sub-menu">
-            <li class="sub-menu-item"><a href="/categories/sport">Спорт</a></li>
-            <li class="sub-menu-item"><a href="/categories/politic">Политика</a></li>
-        </ul>
-    </li>
+    <li class="top-menu-item"><a href="{{ route ('categories') }}">Категории</a></li>
+    <li class="top-menu-item"><a href="{{ route ('news') }}">Новости</a></li>
 </ul>
 <ul class="top-menu right-menu">
     <li class="top-menu-item"><a href="{{ route ('admin-main') }}">Админка</a></li>
